812d352b91d4a42aef2241859a717e13adca9921
alexandre <alexandre.santos@metatron.com.br>
2024-02-04 21:50:00 -0300

    Feature(NHibernate): Mapping for Equipamento created
    Mapping for Equipamento were created at Data.NHibernate.
    
    Feature(NHibernate)

---
+++ Data.NHibernate/Mappings/EquipamentoMap.cs
@@ -0,0 +1,87 @@
+﻿using DesafioFullStack.Domain.Entity;
+using NHibernate;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+
+namespace Data.NHibernate.Mappings
+{
+    public class EquipamentoMap : ClassMapping<Equipamento>
+    {
+        public EquipamentoMap()
+        {
+            Id(b => b.Id, x =>
+            {
+                x.Generator(Generators.Increment);
+                x.Type(NHibernateUtil.Int32);
+                x.Column("Id");
+            });
+
+            Property(b => b.NumeroSerie, x =>
+            {
+                x.Length(150);
+                x.Type(NHibernateUtil.String);
+                x.Column("NumeroSerie");
+            });
+
+            Property(b => b.EnderecoHardware, x =>
+            {
+                x.Length(150);
+                x.Type(NHibernateUtil.String);
+                x.Column("EnderecoHardware");
+            });
+
+            Property(b => b.SimCard, x =>
+            {
+                x.Length(150);
+                x.Type(NHibernateUtil.String);
+                x.Column("SimCard");
+            });
+
+            Property(b => b.Operadora, x =>
+            {
+                x.Length(150);
+                x.Type(NHibernateUtil.String);
+                x.Column("Operadora");
+            });
+
+            Property(b => b.RedeAtiva, x =>
+            {
+                x.Type(NHibernateUtil.Boolean);
+                x.Column("RedeAtiva");
+            });
+
+            Property(b => b.MemoriaDoCartao, x =>
+            {
+                x.Type(NHibernateUtil.Boolean);
+                x.Column("MemoriaDoCartao");
+            });
+
+            Property(b => b.OrgaoGestorID, x =>
+            {
+                x.Type(NHibernateUtil.Int32);
+                x.Column("OrgaoGestorID");
+            });
+
+            Property(b => b.TipoEquipamentoID, x =>
+            {
+                x.Type(NHibernateUtil.Int32);
+                x.Column("TipoEquipamentoID");
+            });
+
+            Property(b => b.FabricanteID, x =>
+            {
+                x.Type(NHibernateUtil.Int32);
+                x.Column("FabricanteID");
+            });
+
+            Property(b => b.IMEI, x =>
+            {
+                x.Length(150);
+                x.Type(NHibernateUtil.String);
+                x.Column("IMEI");
+            });
+
+            Table("Equipamento");
+        }
+    }
+}
--- DesafioFullStack.Domain/Entity/Equipamento.cs
+++ DesafioFullStack.Domain/Entity/Equipamento.cs
@@ -9,17 +9,16 @@ namespace DesafioFullStack.Domain.Entity
     public class Equipamento
     {
         public int Id { get; set; }
-        public string? NomeEquipamento { get; set; }
         public string? NumeroSerie { get; set; }
+        public string? IMEI { get; set; }
         public string? EnderecoHardware { get; set; }
-        public string? EnderecoHadware { get; set; }
         public string? SimCard { get; set; }
         public string? Operadora { get; set; }
         public bool RedeAtiva { get; set; }
         public bool MemoriaDoCartao { get; set; }
-        public OrgaoGestor? OrgaoGestor { get; set; }
-        public TipoEquipamento? TipoEquipamento { get; set; }
-        public Fabricante? Fabricante { get; set; }
+        public int OrgaoGestorID { get; set; }
+        public int TipoEquipamentoID { get; set; }
+        public int FabricanteID { get; set; }
 
     }
 }
